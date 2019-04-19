ceb2a512e4ff571cc166c5f533312327030e496d
pzs <756306696@qq.com>
2019-04-19 09:58:22 +0800

    fragment重叠问题处理

--- commonlib/src/main/java/com/example/commonlib/base/BaseFragment.java
+++ commonlib/src/main/java/com/example/commonlib/base/BaseFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +21,48 @@ import dagger.android.support.AndroidSupportInjection;
 
 public abstract class BaseFragment extends Fragment {
 
+    /**
+     * fragment显示状态标记
+     */
+    private final String STATE_SAVE_IS_HIDDEN = "STATE_SAVE_IS_HIDDEN";
     private Unbinder unbinder;
 
     protected abstract int bindLayout();
+
     protected abstract void initView();
 
     @Override
     public void onAttach(Context context) {
         AndroidSupportInjection.inject(this);
         super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            // 1、处理fragment重叠的问题
+            // 2、解决思路：由Activity/父Fragment来管理子Fragment的Hidden状态 转变为 由Fragment自己来管理自己的Hidden状态！
+            // 3、不管多深的嵌套Fragment、同级Fragment等场景，全都可以正常工作，不会发生重叠！
+            // 4、加载Fragment时，在Activity的onCreate方法或者Fragment的onCreateView方法中通过findFragmentByTag判断一下要加载的fragment是否为空，只有为空才加载；
+            boolean fragmentIsHidden = savedInstanceState.getBoolean(STATE_SAVE_IS_HIDDEN);
+
+            FragmentManager fragmentManager = getFragmentManager();
+
+            FragmentTransaction fragmentTransaction = null;
+            if (fragmentManager != null) {
+                fragmentTransaction = fragmentManager.beginTransaction();
+            }
+
+            if (fragmentTransaction != null) {
+                if (fragmentIsHidden) {
+                    fragmentTransaction.hide(this);
+                } else {
+                    fragmentTransaction.show(this);
+                }
+                fragmentTransaction.commit();
+            }
+        }
     }
 
     @Nullable
@@ -42,6 +77,12 @@ public abstract class BaseFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initView();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_SAVE_IS_HIDDEN, isHidden());
     }
 
     @Override
